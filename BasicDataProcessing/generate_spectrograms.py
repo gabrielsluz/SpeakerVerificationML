7c0fabc15809d7b957ae4fd76d4796a2b68f8f37
gabrielsluz <gabrielsluz1999@gmail.com>
2020-06-10 01:16:55 -0300

    Generate normalized spectrograms in dev

--- BasicDataProcessing/generate_spectrograms.py
+++ BasicDataProcessing/generate_spectrograms.py
@@ -2,6 +2,7 @@ import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 """
 Inputs:
@@ -29,6 +30,41 @@ def mel_spectrograms(audio_list, audio_length_ms, sampling_rate, n_mels, window_
     
     return spectrograms
 
+
+#Same as mel_spectrograms but sets the mean to 0 and variance to 1
+def mel_spectrogramsNormalized(audio_list, audio_length_ms, sampling_rate, n_mels, window_ms, stride_ms):
+    spectrograms = {}
+    stride_size = int(0.001 * sampling_rate * stride_ms)
+    window_size = int(0.001 * sampling_rate * window_ms)
+    max_size = int(0.001 * sampling_rate * audio_length_ms)
+
+    audio, sampling_rate = librosa.load(audio_list[0], sr = None, mono = True, offset = 0.0, duration = None)
+    n_windows = librosa.feature.melspectrogram(audio[:max_size], sr=sampling_rate, n_fft=window_size, hop_length=stride_size, n_mels=n_mels).shape[1]
+    spec_array = np.zeros((len(audio_list), n_mels, n_windows))
+
+    index = 0
+    for audio_path in audio_list:
+        try:
+            audio, sampling_rate = librosa.load(audio_path, sr = None, mono = True, offset = 0.0, duration = None)
+            S = librosa.feature.melspectrogram(audio[:max_size], sr=sampling_rate, n_fft=window_size, hop_length=stride_size, n_mels=n_mels)
+            S_DB = librosa.power_to_db(S, ref=np.max)
+            spec_array[index] = S_DB
+            index += 1
+        except:
+            print(audio_path)
+
+    spec_array = spec_array.reshape((len(audio_list), n_mels * n_windows))
+    scaler = preprocessing.StandardScaler().fit(spec_array)
+    spec_array = scaler.transform(spec_array)
+    spec_array = spec_array.reshape((len(audio_list), n_mels, n_windows))
+
+    index = 0
+    for audio_path in audio_list:
+        spectrograms[audio_path] = spec_array[index]
+        index += 1
+    
+    return spectrograms, scaler
+
 def display_spectrogram(spectrogram, sampling_rate, stride_size):
     librosa.display.specshow(spectrogram, sr=sampling_rate, hop_length=stride_size, x_axis='time', y_axis='mel')
     plt.colorbar(format='%+2.0f dB')
@@ -41,3 +77,7 @@ spectrograms = mel_spectrograms(["TesteAudio.wav"], 3000, 16000, 110, 20, 10)
 print(spectrograms)
 display_spectrogram(spectrograms["TesteAudio.wav"], 16000, stride_size)
 '''
+spectrograms = mel_spectrograms(["TesteAudio.wav", "TesteAudio1.wav"], 3000, 16000, 64, 20, 10)
+print(spectrograms)
+spectrograms = mel_spectrogramsNormalized(["TesteAudio.wav", "TesteAudio1.wav"], 3000, 16000, 64, 20, 10)
+print(spectrograms)
